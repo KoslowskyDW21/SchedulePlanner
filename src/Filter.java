fadd0cfdd4e56c72c3306234770fc76d99991ce0
Jeffrey Krug <krugjl21@gcc.edu>
2024-03-06 13:05:49 -0500

    Getters and Setters for Filter

--- src/Filter.java
+++ src/Filter.java
@@ -23,4 +23,67 @@ public class Filter {
         this.department = "";
     }
 
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getLevelMin() {
+        return levelMin;
+    }
+
+    public void setLevelMin(int levelMin) {
+        this.levelMin = levelMin;
+    }
+
+    public int getLevelMax() {
+        return levelMax;
+    }
+
+    public void setLevelMax(int levelMax) {
+        this.levelMax = levelMax;
+    }
+
+    public String getProfName() {
+        return profName;
+    }
+
+    public void setProfName(String profName) {
+        this.profName = profName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
